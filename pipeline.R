d119f136cb48334892f80584d7b7e1143140ea27
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2023-08-02 11:21:08 +0100

    Almost working, blocked on some orderly changes

--- pipeline.R
+++ pipeline.R
@@ -1,5 +1,8 @@
-#!/usr/bin/env Rscript
+## If not initialised after a clone, you need to run:
+orderly2::orderly_init(".")
+
 ctry_year <- read.csv("params.csv", strip.white = TRUE)
+regions <- read.csv("sites.csv", strip.white = TRUE)
 
 orderly2::orderly_run("prepare_global_inputs")
 
@@ -18,8 +21,6 @@ for (i in seq_len(nrow(ctry_year))) {
 
 # you will probably want to work on the calibrate_region steps locally
 # before attempting to submit to the cluster:
-regions <- read.csv("sites.csv", strip.white = TRUE)
-
 orderly2::orderly_run("calibrate_region",
                       parameters = list(country = regions$country[[1]],
                                         region = regions$region[[1]],
@@ -28,34 +29,43 @@ orderly2::orderly_run("calibrate_region",
 # when running remotely there will be an extra step here to pull dependencies into the orderly root
 # on the network share drive
 # setup cluster tools
-context_root <- "contexts"
-ctx <- context::context_save(context_root)
+ctx <- context::context_save("contexts")
 
 config <- didehpc::didehpc_config(cluster = "big")
 obj <- didehpc::queue_didehpc(ctx, config = config)
 obj$install_packages("mrc-ide/orderly2")
 obj$install_packages("dplyr")
 
-
-
 # queue all calibrations
 bundle <- obj$enqueue_bulk(regions, function(country, region, year) {
   orderly2::orderly_run("calibrate_region",
                         parameters = list(country = country,
                                           region = region,
-                                          year = year),
-                        root = orderly_root)
+                                          year = year))
 })
 
 # extract dependencies
-deps <- apply(ctry_year, 1, function(row) bundle$ids[bundle$X$country == row[[1]] && bundle$X$year == row[[2]]])
+
+deps <- lapply(seq_len(nrow(ctry_year)), function(i) {
+  bundle$ids[bundle$X$country == ctry_year$country[[i]] & bundle$X$year == ctry_year$year[[i]]]
+})
 
 # queue aggregation steps with dependencies
 aggregation_bundle <- obj$enqueue_bulk(ctry_year, function(country, year) {
-  orderly3::orderly_run("final_outputs",
+  orderly2::orderly_run("final_outputs",
                         parameters = list(country = country,
-                                          year = year),
-                        use_draft = TRUE)
+                                          year = year))
 }, depends_on = deps)
 
 # final step would be to push the results to the orderly remote
+
+
+# The last steps could also be run without the cluster:
+for (i in seq_len(nrow(regions))) {
+  orderly2::orderly_run("calibrate_region",
+                        parameters = as.list(regions[i, ]))
+}
+
+for (i in seq_len(nrow(ctry_year))) {
+  orderly2::orderly_run("final_outputs", parameters = as.list(ctry_year[i, ]))
+}
